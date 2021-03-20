16f7331f75d558539748c90b95f5d554676848c1
SyedaNaqvi9 <70096819+SyedaNaqvi9@users.noreply.github.com>
2021-03-20 11:13:04 +1100

    Update bloodbank.sql

--- bloodbank.sql
+++ bloodbank.sql
@@ -12,3 +12,19 @@ email varchar(250) not null,
 bloodgroup varchar(250) not null,
 city varchar(150) not null,
 daddress varchar(550) not null);
+
+create table stock(
+bid int not null identity(1,1) primary key,
+blood_group varchar(50) not null,
+quantity varchar(50) not null);
+
+
+insert into stock(blood_group,quantity) values ('A+',100);
+insert into stock(blood_group,quantity) values ('B+',200);
+insert into stock(blood_group,quantity) values ('AB+',300);
+insert into stock(blood_group,quantity) values ('AB-',300);
+insert into stock(blood_group,quantity) values ('O+',200);
+insert into stock(blood_group,quantity) values ('O-',100);
+insert into stock(blood_group,quantity) values ('A-',400);
+insert into stock(blood_group,quantity) values ('B-',500);
+
